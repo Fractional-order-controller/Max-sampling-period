9d4a6e0e132f7bb8b7d68811b1a4c29e96059fe1
Bo Shang <cnpcshangbo@gmail.com>
2018-10-26 00:17:28 +0800

    IOPID parallel data saved.

--- cal_f_h_selector.m
+++ cal_f_h_selector.m
@@ -1,8 +1,8 @@
 function [omega, f_h ] = cal_f_h_selector(h,controller_type)
 % This function is used to auto-select which function to use.
-if 'FOPD' == controller_type
+if length(controller_type) == 4 %&& 'FOPD' == controller_type
     [omega, f_h]=cal_f_h_fopd(h);
-elseif controller_type == 'IOPID'
+elseif length(controller_type) == 5 %&& 'IOPID' == controller_type
     [omega, f_h]=cal_f_h(h);
 else
     fprintf('Error controller type.');
